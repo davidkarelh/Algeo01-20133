9150dd5f0977735516a6e9d3500fdb9f38d87298
davidkarelhp <13520154@std.stei.itb.ac.id>
2021-09-29 11:25:51 +0700

    Ubah sedikit.

---
+++ src/TubesDriver.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class TubesDriver {
+    public static void main(String[] args) {
+        boolean exit = false;
+        int aksi;
+        Scanner input = new Scanner(System.in);
+        do {
+            System.out.println(
+            """
+            MENU UTAMA
+            1. Sistem Persamaaan Linier
+            2. Determinan
+            3. Matriks balikan
+            4. Interpolasi Polinom
+            5. Regresi linier berganda
+            6. Keluar
+            """);
+            System.out.print("Masukkan pilihan aksi yang mau dilakukan: ");
+            aksi = input.nextInt();
+            if (aksi == 1) {
+                SPL.aksi(input);
+            } else if (aksi == 2) {
+                Determinan.aksi(input);
+            } else if (aksi == 3) {
+                MatriksBalikan.aksi(input);
+            } else if (aksi == 4) {
+                InterpolasiPolinom.aksi(input);
+            } else if (aksi == 5) {
+                Regresi.aksi(input);
+            } else if (aksi == 6) {
+                exit = true;
+                System.out.println("Keluar dari program...");
+            } else {
+                System.out.println("Masukkan tidak valid, ulangi!");
+            }
+        } while(!exit);
+        input.close();
+    }
+}
